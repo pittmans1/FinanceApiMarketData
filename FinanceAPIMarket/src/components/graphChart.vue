f2dd8f23f5dcc97246767e8c016eee2eb14c3842
scott pittman <sp.digital97@gmail.com>
2023-11-08 00:37:08 -0500

    start of the graph and making vue 3 application

---
+++ FinanceAPIMarket/src/components/graphChart.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+<script >
+export default {
+  setup() {
+
+    // expose to template and other options API hooks
+    return {
+
+    }
+  },
+    mounted() {
+        fetch('https://api.finazon.io/latest/datasets?code=bybit&page=0&page_size=100&apikey=95fcb7469df942cb82430fb321d47c5exn')
+        .then((response) => {
+           console.log(response)
+        })
+    }
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ FinanceAPIMarket/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import graph from '../components/graphChart.vue'
+</script>
+
+<template>
+  <main>
+    <graph />
+  </main>
+</template>
